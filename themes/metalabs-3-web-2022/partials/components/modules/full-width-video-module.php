b8ac3526c96b2491605f9c3e5501b14bd284cd72
josip <josip.mucak@bornfight.com>
2022-11-20 13:12:29 +0100

    update full width video module

--- themes/metalabs-3-web-2022/partials/components/modules/full-width-video-module.php
+++ themes/metalabs-3-web-2022/partials/components/modules/full-width-video-module.php
@@ -7,22 +7,22 @@
  *
  */
 
-if (!empty($desktop_url)) {
-    $anchor = !empty($anchor_id) ? 'id=' . $anchor_id : '';
-    ?>
-    <section class="o-section c-full-width-video-module" <?= esc_attr($anchor); ?>
-             data-anchor-target="<?= $anchor_id; ?>">
+if ( ! empty( $desktop_url ) ) {
+	$anchor = ! empty( $anchor_id ) ? 'id=' . $anchor_id : '';
+	?>
+    <section class="o-section c-full-width-video-module" <?= esc_attr( $anchor ); ?>
+             data-anchor-target="<?= esc_attr( $anchor_id ); ?>">
         <div class="o-container o-container--full-width">
             <video
                     class="js-video"
-                    data-mobile="<?= $mobile_url; ?>"
+                    data-mobile="<?= esc_url( $mobile_url ); ?>"
                     disablePictureInPicture
                     loop
                     muted
                     playsInline
                     autoplay
                     preload="none"
-                    src="<?= esc_url($desktop_url); ?>"
+                    src="<?= esc_url( $desktop_url ); ?>"
             ></video>
         </div>
     </section>
--- themes/metalabs-3-web-2022/partials/modules/full_width_video_module.php
+++ themes/metalabs-3-web-2022/partials/modules/full_width_video_module.php
@@ -10,12 +10,15 @@
 
 use metalabs3Web2022\helpers\VideoHelper;
 
-if (!empty($show)) {
-    $video_helper      = new VideoHelper();
-    $video_data        = $video_helper->get_video_data($video);
-    get_partial('components/modules/full-width-video-module', array(
-        'desktop_url' => !empty($video_data['url']) ? $video_data['url'] : '',
-        'mobile_url'  => !empty($video_data['mobile_url']) ? $video_data['mobile_url'] : $video_data['url'],
-        'anchor_id'   => $anchor_id,
-    ));
+if ( ! empty( $show ) ) {
+	$video_helper = new VideoHelper();
+	$video_data   = $video_helper->get_video_data( $video );
+	$desktop_url  = ! empty( $video_data['url'] ) ? $video_data['url'] : '';
+	$mobile_url   = ! empty( $video_data['mobile_url'] ) ? $video_data['mobile_url'] : $desktop_url;
+
+	get_partial( 'components/modules/full-width-video-module', array(
+		'desktop_url' => $desktop_url,
+		'mobile_url'  => $mobile_url,
+		'anchor_id'   => $anchor_id,
+	) );
 }
