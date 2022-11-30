7e064ae8125527188263ec2ff1d2414f7a95e227
josip <josip.mucak@bornfight.com>
2022-11-30 12:44:55 +0100

    add acf field for gtm

--- themes/metalabs-3-web-2022/header.php
+++ themes/metalabs-3-web-2022/header.php
@@ -11,10 +11,10 @@
  * @subpackage angelo
  */
 
-use metalabs3Web2022\bundles\Metalabs3Web2022Assets;
+use metalabs3Web2022\acf\ACFProvider;use metalabs3Web2022\bundles\Metalabs3Web2022Assets;
 use metalabs3Web2022\bundles\Metalabs3Web2022ProductionAssets;
 
-if (defined('LOCAL') && LOCAL === true) {
+if ( defined( 'LOCAL' ) && LOCAL === true ) {
 	Metalabs3Web2022Assets::register();
 } else {
 	Metalabs3Web2022ProductionAssets::register();
@@ -22,19 +22,22 @@ if (defined('LOCAL') && LOCAL === true) {
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<?php if ( ! defined( 'LOCAL' ) ) {
+		echo ACFProvider::get_instance()->get_option_field( 'gtm_-_head' );
+	} ?>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <title>
-		<?= wp_title('') ?>
+		<?= wp_title( '' ) ?>
     </title>
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= bu("ui/favicon/apple-touch-icon.png"); ?>">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= bu("ui/favicon/favicon-32x32.png"); ?>">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= bu("ui/favicon/favicon-16x16.png"); ?>">
-    <link rel="manifest" href="<?= bu("ui/favicon/site.webmanifest"); ?>">
-    <link rel="mask-icon" href="<?= bu("ui/favicon/safari-pinned-tab.svg"); ?>" color="#000000">
-    <link rel="shortcut icon" href="<?= bu("ui/favicon/favicon.ico"); ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= bu( "ui/favicon/apple-touch-icon.png" ); ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= bu( "ui/favicon/favicon-32x32.png" ); ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= bu( "ui/favicon/favicon-16x16.png" ); ?>">
+    <link rel="manifest" href="<?= bu( "ui/favicon/site.webmanifest" ); ?>">
+    <link rel="mask-icon" href="<?= bu( "ui/favicon/safari-pinned-tab.svg" ); ?>" color="#000000">
+    <link rel="shortcut icon" href="<?= bu( "ui/favicon/favicon.ico" ); ?>">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#000000">
 
@@ -42,5 +45,7 @@ if (defined('LOCAL') && LOCAL === true) {
 </head>
 
 <body <?php body_class(); ?>>
-
+<?php if ( ! defined( 'LOCAL' ) ) {
+	echo ACFProvider::get_instance()->get_option_field( 'gtm_-_body' );
+} ?>
 
